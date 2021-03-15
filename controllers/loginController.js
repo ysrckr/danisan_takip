63b98443c4892c7f242a2aa878192eaaec06944e
Yaşar Çakır <yasar@Yasar-MacBook-Pro.local>
2021-03-15 22:36:06 +0300

    login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,8 @@
-
 exports.loginController = (req, res, next) => {
-    res.render('login', {title: 'Welcome to Login', css:['main.min.css', 'login.min.css']})
+	res.render('login', {
+		title: 'Welcome to Login',
+		loginHeader: 'Login',
+		css: ['main.min.css', 'login.min.css'],
+		javascript: ['index.js'],
+	})
 }
